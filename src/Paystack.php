9bd66ce17983536bf3fb94617700da3a77047237
OniiCoder <oniicoder@gmail.com>
2023-04-30 00:39:14 +0000

    fix indentation

--- src/Paystack.php
+++ src/Paystack.php
@@ -29,25 +29,24 @@ class Paystack
 
         $fields_string = http_build_query($fields);
 
-        //open connection
+        // open connection
         $ch = curl_init();
 
         $secretKey = settings(PaystackSettings::class, 'secret_key');
 
-        //set the url, number of POST vars, POST data
+        // set the url, number of POST vars, POST data
         curl_setopt($ch,CURLOPT_URL, $url);
         curl_setopt($ch,CURLOPT_POST, true);
         curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//            "Authorization: Bearer SECRET_KEY",
             "Authorization: Bearer ".$secretKey,
             "Cache-Control: no-cache",
         ));
 
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
+        // So that curl_exec returns the contents of the cURL; rather than echoing it
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
-        //execute post
+        // execute post
         $result = curl_exec($ch);
         return $result;
     }
@@ -64,7 +63,6 @@ class Paystack
         // set the url
         curl_setopt($ch,CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//            "Authorization: Bearer SECRET_KEY",
             "Authorization: Bearer ".$secretKey,
         ));
 
